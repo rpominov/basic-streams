c70060be6b3e86f19c13f90d06aadc0bc2a8e299
Roman Pominov <rpominov@gmail.com>
2016-01-06 16:35:07 +0300

    minor updates in FL wrapper

--- src/fantasy.js
+++ src/fantasy.js
@@ -7,16 +7,34 @@ export class Stream<T> {
 
   observe: BasicStream<T>;
 
+  // Example:
+  //
+  //    const myWrappedStream = new Stream(sink => {
+  //      sink(1)
+  //      retrun () => {}
+  //    })
+  //
   constructor( basicStream:BasicStream<T> ) {
+
+    // The wrapped BasicStream is available as `wrapped.observe`.
+    // You can call it as a method in order to subscribe to the stream:
+    //
+    //    const usub = wrapped.observe(callback)
+    //    unsub()
+    //
+    // Or you can use `wrapped.observe` to extract BasicStream from the wrapper e.g.:
+    //
+    //    const basicStream2 = lift(x => x * 2)(wrapped.observe)
+    //
     this.observe = basicStream
   }
 
-  // Monoid
-  empty(): Stream<any> {
-    return new Stream(bs.empty)
-  }
+  // Monoid (we don't have .concat though)
   static empty(): Stream<any> {
     return new Stream(bs.empty)
+  }
+  empty(): Stream<any> {
+    return Stream.empty()
   }
 
   // Functor
@@ -33,11 +51,11 @@ export class Stream<T> {
   }
 
   // Applicative
-  of<A>( x:A ): Stream<A> {
-    return new Stream(bs.just(x))
-  }
   static of<A>( x:A ): Stream<A> {
     return new Stream(bs.just(x))
+  }
+  of<A>( x:A ): Stream<A> {
+    return Stream.of(x)
   }
 
   // Chain
