49f61cbb3c74f2f169618cb6aa124f82edf4da38
Roman Pominov <rpominov@gmail.com>
2016-02-21 18:15:50 +0300

    more generic async-problem example

--- examples/async-problem/index.js
+++ examples/async-problem/index.js
@@ -1,7 +1,8 @@
 import {readFile} from 'fs'
 import {join} from 'path'
-import {Success, Failure} from 'data.validation'
-import {map, chain, just, combineArray} from 'basic-streams'
+import Validation, {Success, Failure} from 'data.validation'
+import R from 'ramda'
+import * as BS from 'basic-streams'
 import {pipe} from 'basic-streams/lib/utils'
 
 
@@ -19,14 +20,27 @@ const liftNodeFn = nodeFn => (...args) =>
 
 
 // This could be in data.validation
-const vLift2 = fn => (vA, vB) => vA.map(a => b => fn(a, b)).ap(vB)
-const vCombineArray = vArr => vArr.reduce(vLift2((arr, x) => arr.concat([x])), Success([]))
+Validation.prototype.sequence = function(of) {
+  return this.fold(
+    e => of(Failure(e)),
+    x => BS.map(Success)(x)  // nevermind specialisation for BS here, we will use FL wrapper for BS
+  )
+}
+Validation.prototype.chain = function(fn) {
+  return this.map(fn).getOrElse(this)
+}
 
 
 // Helpers we need to work with `Stream(Validation([e], a))` type
-const mapV = fn => map(v => v.map(fn))
-const combineArrayV = arr => map(vCombineArray)(combineArray(arr))
-const chainV = fn => chain(v => v.fold(e => just(Failure(e)), fn))
+const mapInner = fn => BS.map(R.map(fn))
+const combineArrayInner = of => arr => BS.map(R.sequence(of))(BS.combineArray(arr))
+const chainInner = fn => s =>
+                            BS.chain(v => {
+                              const vsv = v.map(fn)
+                              const svv = vsv.sequence(BS.just)
+                              const sv = BS.map(vv => vv.chain(x => x))(svv)
+                              return sv
+                            })(s)
 
 
 // Done with helpers, the folowing is our app logic
@@ -36,10 +50,10 @@ const readFileLifted = liftNodeFn(readFile)
 const main = dir => {
   const readFileFromDir = name => readFileLifted(join(dir, name), {encoding: 'utf8'})
 
-  const stream = pipe(readFileFromDir('index'),                      // Stream(Val([e], string))
-    mapV(index => index.match(/^.*(?=\n)/gm).map(readFileFromDir)),  // Stream(Val([e], [Stream(Val([e], string))]))
-    chainV(combineArrayV),                                           // Stream(Val([e], [string]))
-    mapV(arr => arr.join(''))                                        // Stream(Val([e], string))
+  const stream = pipe(readFileFromDir('index'),                          // Stream(Val([e], string))
+    mapInner(index => index.match(/^.*(?=\n)/gm).map(readFileFromDir)),  // Stream(Val([e], [Stream(Val([e], string))]))
+    chainInner(combineArrayInner(Success)),                              // Stream(Val([e], [string]))
+    mapInner(arr => arr.join(''))                                        // Stream(Val([e], string))
   )
 
   stream(v => v.fold(
