22db0dd2964cfe08c577facc52162c4485581355
Roman Pominov <rpominov@gmail.com>
2016-09-26 10:14:18 +0300

    remove forgotten FantasyStream export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import Stream from './main'
-import {Stream as FantasyStream} from './fantasy'
 import * as marbles from './marbles'
 
-export {Stream, FantasyStream, marbles}
+export {Stream, marbles}
